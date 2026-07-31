e009f153f75005dab62bf56a5afb212e0e0032ef
Thana <chua.thanaseth@gmail.com>
2026-08-01 03:47:20 +0700

    Update references after DAG file renames

--- tests/test_airflow_execution_metadata.py
+++ tests/test_airflow_execution_metadata.py
@@ -3,7 +3,7 @@ from types import SimpleNamespace
 
 import pytest
 
-from dags import vendor_payments_etl_orchestration as orchestration
+from dags import vendor_payments_data_platform_orchestration as orchestration
 
 
 def build_task_instance(
